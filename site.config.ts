1141a814c65f9095600d268504eae85274a1fa13
cheun10 <cheun11003@gmail.com>
2024-06-09 20:20:41 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '신채은 이력',
+  domain: 'chaeeun.vercel.app',
+  author: '신채은',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
